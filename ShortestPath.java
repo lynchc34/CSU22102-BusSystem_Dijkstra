e52c149664b53afaa98be496f1015f12ab3352e0
Matthew Dowse <dowsema@tcd.ie>
2021-04-28 20:37:18 +0100

    Updated ShortestPath

--- ShortestPath.java
+++ ShortestPath.java
@@ -2,17 +2,31 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
+//class to create graph, add edges
+//and find shortest path
+//by Matthew Dowse 19333534
 public class ShortestPath {
 
-	private String from;
-	private String to;
 	String line = null;
 	private int N;		//total number of intersections
 	private int S;		//total number of streets
 	private EWDGraph graph;
 
+	private double[] distTo;          // distTo[v] = distance  of shortest s->v path
+	private DirectedEdge[] edgeTo;    // edgeTo[v] = last edge on shortest s->v path
+	private IndexMinPQ<Double> pq;    // priority queue of vertices
+
 	//constructor to populate graph (Bus Routes/city plan)
+	//NOTE use "stop_times.txt"
 	ShortestPath(String filename){
+
+		N = totalIntersections(filename);	//gets total Intersections N 
+		S = totalStreets(filename);
+		graph = new EWDGraph(N, S);			//create graph with N intersections and S streets
+		String line = null;
+		String[] street = {}; 
+		String[] street2 = {}; 
+		//NOTE NEED TO take into account transfer type/cost/weight etc
 
 		try {
 
@@ -21,11 +35,21 @@ public class ShortestPath {
 				File f = new File(filename);
 				Scanner s1 = new Scanner(f);
 
-				N = totalIntersections(filename);
-				S = totalStreets(filename);
+				for(int i=0; s1.hasNextLine(); i++)
+				{
 
-				graph = new EWDGraph(N, S);			//create graph with N intersections and S streets
+					if(i==0) continue;			//ignore first line of file with details
 
+					street = line.trim().split(("\\s+"));
+					street2 = line.trim().split(("\\s+"));
+
+					if(street[0] == street2[0])	//share the same trip_id
+					{
+						graph.addEdge(new DirectedEdge(Integer.parseInt(street[4]), Integer.parseInt(street2[4]),
+								Double.parseDouble(street[9]))); //edit for weight/cost
+					}
+
+				}s1.close();
 			}
 		}catch(FileNotFoundException e) {
 			e.printStackTrace();
@@ -33,28 +57,29 @@ public class ShortestPath {
 
 	}
 
-
-	//user inputs two bus stops
-	//checks if they are on the same line/ trip I.D
-	public String twoStops(String from, String to)
+	//NOTE use "stop_times.txt" to use trip_ID
+	public double twoIntStops(int from, int to)
 	{
+		double distance = 0;
 
-		this.from = from;
-		this.to = to;
+		//get trip_id to check if they share the same trip_id
 
-		return to;
+		dijkstraSP(graph, from);
+		distance = distTo(to);
 
+		return distance;
 	}
 
 	//get total amount of Intersections
 	//NOTE - Might need to edit as some intersections are the same
+	//NOTE NOTE might not be needed ^
+	//READ IN FILE "stops.txt"
 	//i.e from_stop = to_stop
 	public int totalIntersections(String filename)
 	{
 		int totalAmount = 0;
 
 		try {
-
 			if(filename != null)
 			{
 				File f = new File(filename);
@@ -72,15 +97,16 @@ public class ShortestPath {
 		}
 		return totalAmount-1;	//return total amount of lines -1 (first line is the title) 
 	}
-	
-	
+
+
 	//get total amount of streets
 	//adds 1 to totalAmount for each line
 	//remembering to subtract 1 from totalAmount as line 1 = title 
+	//READ IN FILE "transfers.txt" as each line is an edge
 	public int totalStreets(String filename)
 	{
 		int totalAmount = 0;
-		
+
 		try {
 
 			if(filename != null)
@@ -99,7 +125,44 @@ public class ShortestPath {
 		}
 		return totalAmount-1;
 	}
-	
-	
+
+	//Implementation of Dijkstra's Shortest Path Algorithm
+	//Code based from Algorithms 4th Edition by Robert Sedgewick and Kevin Wayne
+	//https://algs4.cs.princeton.edu/code/edu/princeton/cs/algs4/DijkstraSP.java.html
+	private void dijkstraSP(EWDGraph G, int s) {
+		// TODO Auto-generated method stub
+
+		distTo = new double[G.V()];
+		edgeTo = new DirectedEdge[G.V()];
+
+		for (int v = 0; v < G.V(); v++)
+			distTo[v] = Double.POSITIVE_INFINITY;
+		distTo[s] = 0.0;
+
+		// relax vertices in order of distance from s
+		pq = new IndexMinPQ<Double>(G.V());
+		pq.insert(s, distTo[s]);
+		while (!pq.isEmpty()) {
+			int v = pq.delMin();
+			for (DirectedEdge e : G.adj(v))
+				relax(e);
+		}
+	}
+
+	// relax edge e and update pq if changed
+	private void relax(DirectedEdge e) {
+		int v = e.from(), w = e.to();
+		if (distTo[w] > distTo[v] + e.weight()) {
+			distTo[w] = distTo[v] + e.weight();
+			edgeTo[w] = e;
+			if (pq.contains(w)) pq.decreaseKey(w, distTo[w]);
+			else                pq.insert(w, distTo[w]);
+		}
+	}
+
+	public double distTo(int v) {
+		return distTo[v];
+	}
+
 
 }
