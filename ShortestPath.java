41cd09f1cf6ce7d59647f81fc73f784b360d2378
Matthew Dowse <dowsema@tcd.ie>
2021-04-28 14:40:16 +0100

    Updated Shortest Path

--- ShortestPath.java
+++ ShortestPath.java
@@ -7,6 +7,9 @@ public class ShortestPath {
 	private String from;
 	private String to;
 	String line = null;
+	private int N;		//total number of intersections
+	private int S;		//total number of streets
+	private EWDGraph graph;
 
 	//constructor to populate graph (Bus Routes/city plan)
 	ShortestPath(String filename){
@@ -18,9 +21,10 @@ public class ShortestPath {
 				File f = new File(filename);
 				Scanner s1 = new Scanner(f);
 
+				N = totalIntersections(filename);
+				S = totalStreets(filename);
 
-
-
+				graph = new EWDGraph(N, S);			//create graph with N intersections and S streets
 
 			}
 		}catch(FileNotFoundException e) {
@@ -59,15 +63,43 @@ public class ShortestPath {
 				for(int i=0; s1.hasNextLine(); i++)
 				{
 					line = s1.nextLine();
+					//if statement to check if stop does have a transfer type
 					totalAmount++;
 				}
 			}
 		}catch(FileNotFoundException e) {
 			e.printStackTrace();
 		}
-
-
 		return totalAmount-1;	//return total amount of lines -1 (first line is the title) 
 	}
+	
+	
+	//get total amount of streets
+	//adds 1 to totalAmount for each line
+	//remembering to subtract 1 from totalAmount as line 1 = title 
+	public int totalStreets(String filename)
+	{
+		int totalAmount = 0;
+		
+		try {
+
+			if(filename != null)
+			{
+				File f = new File(filename);
+				Scanner s1 = new Scanner(f);
+
+				for(int i=0; s1.hasNextLine(); i++)
+				{
+					line = s1.nextLine();
+					totalAmount++;
+				}
+			}
+		}catch(FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		return totalAmount-1;
+	}
+	
+	
 
 }
